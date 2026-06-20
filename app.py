34264f49f454095ebbb0222825b19b6ac6150a14
AtharvaGhongade <152916838+AtharvaGhongade@users.noreply.github.com>
2026-06-20 11:30:36 +0530

    Create initial Flask app with basic routes
    Initialize a basic Flask application with routes for the home page and health check.

---
+++ app.py
@@ -0,0 +1,17 @@
+# This code is from https://github.com/LondheShubham153/flask-app-ecs/tree/main
+# Flask App
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+  
+if nne:
+    go
+
+
+@app.route('/health')
+def health():
+    return 'Server is up and running'
